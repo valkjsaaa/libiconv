6e50b2350128f6aea063d429ab93f6c6b6df09be
Jackie Yang <valkjsaaa@users.noreply.github.com>
2016-11-06 18:34:02 -0800

    Update stdio.in.h
    fix build problem for older glibc

--- source/srclib/stdio.in.h
+++ source/srclib/stdio.in.h
@@ -695,7 +695,9 @@ _GL_CXXALIASWARN (gets);
 /* It is very rare that the developer ever has full control of stdin,
    so any use of gets warrants an unconditional warning.  Assume it is
    always declared, since it is required by C89.  */
+# if defined(__GLIBC__) && !defined(__UCLIBC__) && !__GLIBC_PREREQ(2, 16)
 _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
+# endif
 #endif
 
 
